7c525c243a01eb3474c8a0db55d7c8caf4a1fb6e
gavinhigh <gavinhighcollege@gmail.com>
2025-11-15 15:13:34 -0500

    add sqrt and hypotenuse

--- calculator.py
+++ calculator.py
@@ -30,3 +30,13 @@ def log(a, b):
 def exp(a, b):
     return a ** b
 
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take the square root of a negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
+
