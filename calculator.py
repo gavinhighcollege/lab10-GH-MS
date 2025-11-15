21577e43a8e92c3ea52460e2616498cd3110b19a
mikel salomon <mikelsalomon29@gmail.com>
2025-11-15 15:12:26 -0500

    part 1

--- calculator.py
+++ calculator.py
@@ -29,6 +29,7 @@ def div(a, b):
 def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid base or argument for logarithm")
+
     return math.log(b, a)
 
 def exp(a, b):
