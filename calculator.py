0b55a929aa4b269f07d89328fb5833ba2b41b4ae
mikel salomon <mikelsalomon29@gmail.com>
2025-11-15 14:53:53 -0500

    part 1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid base or argument for logarithm")
+
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
